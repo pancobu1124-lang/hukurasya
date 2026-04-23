c33f1378dcfc0c1715ff5530215e6157142722c7
pancobu1124-lang <panco.bu.1124@gmail.com>
2026-04-23 23:25:13 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -52,3 +52,54 @@ $(window).on('load', function () {
 $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
     $('.slider').slick('slickPlay');
 });
+
+
+
+
+
+// タイトルのテキストが動くアニメーション
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".eachTextAnime");
+	element.each(function () {
+		var text = $(this).text();
+		var textbox = "";
+		text.split('').forEach(function (t, i) {
+			if (t !== " ") {
+				if (i < 10) {
+					textbox += '' + t + '';
+				} else {
+					var n = i / 10;
+					textbox += '' + t + '';
+				}
+
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+	});
+
+	EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
